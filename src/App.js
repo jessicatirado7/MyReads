cdca9e33da51d8779f90f9a902927bccaff28772
Jessica Tirado <jessicasoto7@gmail.com>
2018-10-02 13:27:59 -0500

    Added main components to the main page and added the search components to the search page.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+import { Route } from 'react-router-dom'
 
+import mainPage from './components/pages/MainPage';
+import searchPage from './components/pages/SearchPage';
+
+class BooksApp extends React.Component {
   render() {
+    return (
+      <div>
+        <Route exact path="/" component= { mainPage } />
+        <Route exact path="/search" component= { searchPage } />
+      </div>
+    )
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
