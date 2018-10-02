6de508c107cec8e8a020c5093665b129b9bbc64d
Jessica Tirado <jessicasoto7@gmail.com>
2018-10-02 13:36:12 -0500

    Removed page render

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class mainPage extends React.Component {
   render() {
@@ -159,7 +160,7 @@ class mainPage extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book </Link>
         </div>
       </div>
     );
